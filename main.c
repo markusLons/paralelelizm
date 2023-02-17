7134d75f47d49c6d05b9c20856d8a7b7c13c0af4
markuslons <markuslons@gmail.com>
2023-02-17 13:32:38 +0700

    test

--- main.c
+++ main.c
@@ -3,34 +3,22 @@
 #include <math.h>
 
 int N=10000000;
-int rat(double* arr){
 #pragma acc kernels
 
-    for (int i=0; i<N; i++){
-        arr[i] = sin(2*M_PI*i/N);
-    }
-    return 0;
-}
-//Высчитывание суммы
-double sum(double* arr){
-    double sum = 0;
-    for (int i=0; i<N; i++){
-        sum = sum + arr[i];
-    }
-    return sum;
-}
 
 
 int main() {
     double* arr = (double*)malloc((N+1)*sizeof(double));
     // создание массива для видеокарты
+        for (int i=0; i<N; i++){
+        arr[i] = sin(2*M_PI*i/N);
+    }
+    double sum = 0;
+    for (int i=0; i<N; i++){
+        sum = sum + arr[i];
+    }
+    printf("%.50f", sum);
 
 
-
-
-
-    rat(arr);
-    float f = sum(arr);
-    printf("%.50f", f);
     return 0;
 }
