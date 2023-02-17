e54f40e36fca3ce173967c01baa5f6a290e1449e
markuslons <markuslons@gmail.com>
2023-02-17 13:38:16 +0700

    test

--- main.c
+++ main.c
@@ -2,23 +2,22 @@
 #include <malloc.h>
 #include <math.h>
 
-int N=10000000;
-#pragma acc kernels
-
+int N = 10000000;
 
 
 int main() {
-    double* arr = (double*)malloc((N+1)*sizeof(double));
+
+    double *arr = (double *) malloc((N + 1) * sizeof(double));
     // создание массива для видеокарты
-        for (int i=0; i<N; i++){
-        arr[i] = sin(2*M_PI*i/N);
+#pragma acc kernels
+    for (int i = 0; i < N; i++) {
+        arr[i] = sin(2 * M_PI * i / N);
     }
     double sum = 0;
-    for (int i=0; i<N; i++){
+#pragma acc kernels
+    for (int i = 0; i < N; i++) {
         sum = sum + arr[i];
     }
     printf("%.50f", sum);
-
-
     return 0;
 }
