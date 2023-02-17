1dcd404ac770e050c957a9efb0b24eab91eb3a7d
markuslons <markuslons@gmail.com>
2023-02-17 22:36:19 +0700

    test

--- main.c
+++ main.c
@@ -9,41 +9,29 @@ int N = 10000000;
 int main() {
 
     double *arr = (double *) malloc((N + 1) * sizeof(double));
+    clock_t start = clock();
+    double sums = 0;
     // создание массива для видеокарты
 #pragma acc kernels
-    clock_t start = clock();
     for (int i = 0; i < N; i++) {
-        arr[i] = sin(2 * M_PI * i / N);
+        sums += sin(2 * M_PI * i / N);
     }
     clock_t end = clock();
     printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    double sum = 0;
-#pragma acc kernels
-    start = clock();
-    for (int i = 0; i < N; i++) {
-        sum = sum + arr[i];
-    }
-    end = clock();
-    printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    printf("%.50f\n", sum);
+    printf("%.50f\n", sums);
+
 
     // То же самое с float
+    start = clock();
     float *arr2 = (float *) malloc((N + 1) * sizeof(float));
+    float sums2 = 0;
     #pragma acc kernels
-    start = clock();
     for (int i = 0; i < N; i++) {
-        arr2[i] = sin(2 * M_PI * i / N);
+        sums2 += sin(2 * M_PI * i / N);
     }
     end = clock();
     printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    float sum2 = 0;
-    #pragma acc kernels
-    start = clock();
-    for (int i = 0; i < N; i++) {
-        sum2 = sum2 + arr2[i];
-    }
-    end = clock();
-    printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    printf("%.50f\n", sum2);
+
+    printf("%.50f\n", sums2);
     return 0;
 }
